be96a3c599d57e7354c10a1f6bd63455be478288
Bill Fitzgerald <bill@funnymonkey.com>
2021-09-09 11:15:16 -0400

    Initial pass at switching to DDG Tracker Radar

--- analyse_urls.py
+++ analyse_urls.py
@@ -24,13 +24,19 @@ generate_html = "yes"
 output_md = "results/summary.md" # this directory must exist
 output_html = "results/summary.html" # this directory must exist
 url_source = "./source"
-vendors_json = './murl_source/vendors_full.json'
 
 # Create a whitelist of domains to omit from results
 domain_whitelist_base = ['mozilla.org', 'firefox.com', 'mozilla.net', 'mozilla.com']
-subdomain_whitelist_base = ['safebrowsing.googleapis.com']
+subdomain_whitelist_base = ['safebrowsing.googleapis.com'] # need to implement this
 domain_whitelist_bespoke = [] # add additional domains to omit from results 
 domain_whitelist = domain_whitelist_base + domain_whitelist_bespoke
+
+##########################################
+# Do not edit any values below this line #
+##########################################
+
+vendor_base = "./tracker-radar/entities/"
+domains_base = "./tracker-radar/domains/US/"
 
 known_dupes_source = './murl_source/known_dupes.csv'
 known_dupes_list = []
@@ -46,49 +52,48 @@ site_list = []
 domain_list = []
 subdomain_list = []
 
-df_vendors_flat = pd.DataFrame(columns=['owner', 'main_id', 'privacy_policy', 'base_url', 'data_purpose_list'])
-df_domains_base = pd.DataFrame(columns=['site_name', 'base_url', 'subdomain', 'encrypted', 'parameters', 'match_id', 'url'])
-df_domains_enchilada = pd.DataFrame(columns=['site_name', 'base_url', 'subdomain', 'encrypted', 'parameter', 'match_id', 'url', 'owner', 'privacy_policy', 'data_purpose_list'])											
+df_vendors_lookup = pd.DataFrame(columns=['basefile', 'owner', 'base_url', 'lookupfile'])
+df_vl = pd.DataFrame(columns=['basefile', 'owner', 'domain', 'lookupfile'])
+df_domains_enchilada = pd.DataFrame(columns=['site_name', 'base_url', 'subdomain', 'encrypted', 'parameter', 'url', 'owner', 'privacy_policy'])											
 df_report = pd.DataFrame(columns=['site_name', 'report_txt', 'section'])
 
 def create_text(filename,text):
 	with open (filename, 'a') as file:
 		file.write(text)
 
-with open(vendors_json) as input:
-	data = json.load(input)
-	orgs = data['organizations']
-	vendors_dataframe = pd.json_normalize(orgs)
-
-df_vendors=vendors_dataframe.convert_dtypes()
-#print(df_vendors.dtypes)
-
-# prepare vendor list organized by associated_urls
-
-for a, b in df_vendors.iterrows():
-	for al in b['associated_urls']:
-		data_purpose_list = []
-		owner = b['organization_name']
-		main_id = b['main_id']
-		privacy_policy = b['policy_url']
-		base_url = al
-		for x in b['other_data_tools']:
-			data_purpose_list.append(x)
-		for x in b['purp_consent']:
-			data_purpose_list.append(x)
-		for x in b['purp_li']:
-			data_purpose_list.append(x)
-		for x in b['purp_spec_purp']:
-			data_purpose_list.append(x)
-		for x in b['purp_feat']:
-			data_purpose_list.append(x)
-		for x in b['purp_spec_feat']:
-			data_purpose_list.append(x)
-		vf_obj = pd.Series([owner, main_id, privacy_policy, base_url, data_purpose_list], index=df_vendors_flat.columns)
-		df_vendors_flat = df_vendors_flat.append(vf_obj, ignore_index=True)
-
-file_ext = "*.txt" # only get txt files - adjust as needed, set to *.* for all
+## create lookup records for domains
+file_ext = "*.json" 
 count = 0
+sum_count = 0
+
+for path, subdirs, files in os.walk(vendor_base):
+	for f in files:
+		if fnmatch(f,file_ext):
+			fn = os.path.join(path,f)
+			with open(fn) as input:
+				count += 1
+				data = json.load(input)
+				owner = data['name']
+				basefile = f
+				dom_list = data['properties']
+				for d in dom_list:
+					lookupfile = d + ".json"
+					d_obj = pd.Series([basefile, owner, d, lookupfile], index=df_vendors_lookup.columns)
+					df_vendors_lookup = df_vendors_lookup.append(d_obj, ignore_index=True)
+				if count == 500:
+					sum_count = sum_count + count
+					count = 0
+					print(f"Processed {sum_count} records.")
+				else:
+					pass
+		else:
+			pass
+sum_count = sum_count + count
+print(f"\nFinished processing {sum_count} records in total. Moving on to do some real work.\n")
+count = 0
+sum_count = 0
+
+file_ext = "*.txt" 
 
 for path, subdirs, files in os.walk(url_source):
 	for f in files:
@@ -126,7 +131,7 @@ for path, subdirs, files in os.walk(url_source):
 						url = line
 					ext = tldextract.extract(url)
 					base_url = ext.registered_domain
-					if base_url not in domain_whitelist: # add check here to make sure that the base_url is in the vendor list
+					if base_url not in domain_whitelist:
 						if ext.subdomain == "www" or len(ext.subdomain)< 1:
 							subdomain = "None"
 						else: 
@@ -137,22 +142,38 @@ for path, subdirs, files in os.walk(url_source):
 						else:
 							parameter = "No parameters"
 
-						df_vendor_data = df_vendors_flat[df_vendors_flat['base_url'] == base_url]
+						df_vendor_data = df_vendors_lookup[df_vendors_lookup['base_url'] == base_url]
 						if df_vendor_data.shape[0] > 0:
 							for a, b in df_vendor_data.iterrows():
 								owner = b['owner']
-								match_id = b['main_id']
-								privacy_policy = b['privacy_policy']
-								data_purpose_list = b['data_purpose_list']
-								domain_obj = pd.Series([site_name, base_url, subdomain, encrypted, parameter, match_id, url, owner, privacy_policy, data_purpose_list], index=df_domains_enchilada.columns)
-								df_domains_enchilada = df_domains_enchilada.append(domain_obj, ignore_index=True)
-
+								vendor_info = b['lookupfile']
+								vfile_path = os.path.join(domains_base,vendor_info)
+								if os.path.isfile(vfile_path) == True:
+									with open(vfile_path) as vd:
+										vendata = json.load(vd)
+										x = vendata['owner']
+										pp_count = 0
+										for y in x:
+											if y == "privacyPolicy":
+												privacy_policy = x[y]
+												pp_count += 1
+											else:
+												pass
+										if pp_count == 0:
+											privacy_policy = "none listed"
+										print(privacy_policy)
+										# 
+										# for each owner:
+										# filter on privacy policy, then list:
+										# base_url, subdomains, encrypted or not
+										domain_obj = pd.Series([site_name, base_url, subdomain, encrypted, parameter, url, owner, privacy_policy], index=df_domains_enchilada.columns)
+										df_domains_enchilada = df_domains_enchilada.append(domain_obj, ignore_index=True)
+								else:
+									print(f"\nNo matching file for {vfile_path}\n")
 						else: 
 							owner = "unknown"
-							match_id = "none"
 							privacy_policy = ""
-							data_purpose_list = []
-							domain_obj = pd.Series([site_name, base_url, subdomain, encrypted, parameter, match_id, url, owner, privacy_policy, data_purpose_list], index=df_domains_enchilada.columns)
+							domain_obj = pd.Series([site_name, base_url, subdomain, encrypted, parameter, url, owner, privacy_policy], index=df_domains_enchilada.columns)
 							df_domains_enchilada = df_domains_enchilada.append(domain_obj, ignore_index=True)
 
 						if site_name not in site_list:
@@ -172,16 +193,18 @@ for path, subdirs, files in os.walk(url_source):
 site_list.sort()
 domain_list.sort()
 
-#df_domains_enchilada.to_csv("domains_enchilada.csv", encoding='utf-8', index=False)
+print(site_list)
+print(domain_list)
 
-del [[df_vendors, vendors_dataframe]]
+df_domains_enchilada.to_csv("domains_enchilada.csv", encoding='utf-8', index=False)
+
+
+del [df_vendors_lookup]
 gc.collect()
-df_domains_base=pd.DataFrame()
-df_vendors=pd.DataFrame()
-df_vendors_dataframe = pd.DataFrame()
+df_vendors_lookup=pd.DataFrame()
 
 df_match = df_domains_enchilada[df_domains_enchilada['owner'] != "unknown"]
-df_no_match = df_domains_enchilada[df_domains_enchilada['owner'] == "unknown"]
+df_no_match = df_domains_enchilada[df_domains_enchilada['owner'] == "unknown"] 
 
 df_match = df_match.sort_values(by='owner', ascending="true")
 df_no_match = df_no_match.sort_values(by='base_url', ascending="true")
@@ -189,7 +212,7 @@ df_no_match = df_no_match.sort_values(by='base_url', ascending="true")
 ## Top level summary of all tests.
 found_domains = df_domains_enchilada['base_url'].nunique()
 matched_domains = df_match['base_url'].nunique()
-matched_owners = df_match['match_id'].nunique()
+matched_owners = df_match['owner'].nunique()
 top_level_txt = f"# Testing Summary\n\nIn this testing, {len(site_list)} sites were tested. During testing, {found_domains} third parties were called. {matched_domains} of these third parties are connected to {matched_owners} owners."
 top_level_txt = top_level_txt + f"\nSites examined include:\n\n"
 for s in site_list:
@@ -213,8 +236,8 @@ for s in site_list:
 	df_domains_unmatched = df_no_match[df_no_match['site_name'].str.contains(s)]
 
 	matched_domains = df_domains_site['base_url'].nunique()
-	matched_owners = df_domains_site['match_id'].nunique()
-	matched_owners_list = df_domains_site.match_id.unique()
+	matched_owners = df_domains_site['owner'].nunique()
+	matched_owners_list = df_domains_site.owner.unique()
 	unmatched_domains = df_domains_unmatched['base_url'].nunique()
 	unmatched_domains_list = df_domains_unmatched.base_url.unique()
 	unmatched_domains_list.sort()
@@ -232,7 +255,7 @@ for s in site_list:
 
 	for m in matched_owners_list:
 		owner_count += 1
-		df_own = df_domains_site[df_domains_site['match_id'] == m]
+		df_own = df_domains_site[df_domains_site['owner'] == m]
 		owner = df_own['owner'].iloc[0]
 		site_intro_txt = site_intro_txt + f" * {owner}\n"
 		owners_txt = f"### {owner_count}. {owner}\n\n"
@@ -241,16 +264,7 @@ for s in site_list:
 			owners_txt = owners_txt + f'[Privacy policy]({privacy_policy} "Privacy policy for {owner}")\n\n'
 		else:
 			pass
-		owners_txt = owners_txt + "#### Data purpose\n\n"
-		data_p = df_own['data_purpose_list'].iloc[0]
-		if len(data_p) > 1:
-			for dap in data_p:
-				if dap != '[]' and dap != "None":
-					owners_txt = owners_txt + f" * {dap}\n"
-				else:
-					pass
-		else:
-			owners_txt = owners_txt + "No info.\n"
+
 		own_doms = df_own.base_url.unique()
 		own_doms.sort()
 		owners_txt = owners_txt + "\n#### Domains contacted\n\n"
@@ -333,4 +347,3 @@ if generate_html == 'yes':
 		text = f.read()
 		html = markdown.markdown(text)
 	create_text(output_html, html)
-	
